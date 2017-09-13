e08d9ad5db72a6ed22a203ad8e17dc3249b35125
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-09-13 12:32:47 +0530

    Set up react-router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from './components/app'
 import './css/bootstrap.css'
 import './css/index.css'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Route path='/' component={App} />
+  </Router>,
   document.getElementById('root')
 )
 registerServiceWorker()
