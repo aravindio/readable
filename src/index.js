7f6bd73760e551bf9b73ae3d2ac653b054aa08f4
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-09-13 12:37:58 +0530

    Set up reducers and store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import App from './components/app'
+import reducer from './reducers'
 import './css/bootstrap.css'
 import './css/index.css'
 import registerServiceWorker from './registerServiceWorker'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+)
+
 ReactDOM.render(
-  <Router>
-    <Route path='/' component={App} />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Route path='/' component={App} />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 )
 registerServiceWorker()
