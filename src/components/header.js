a4cc32b5f4195db3bad06485da2733288a9d9d43
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-09-19 10:53:41 +0530

    Refactor Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
-class Header extends Component {
-  render() {
-    const { categories } = this.props
-    return (
-      <div className='app'>
-        <Navbar inverse collapseOnSelect={true}>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to='/'>Readable</Link>
-            </Navbar.Brand>
-            {categories && <Navbar.Toggle />}
-          </Navbar.Header>
-          {categories && (
-            <Navbar.Collapse>
-              <Nav pullRight>
-                {categories.map(c => (
-                  <LinkContainer exact key={c.path} to={`/${c.path}`}>
-                    <NavItem>{c.name}</NavItem>
-                  </LinkContainer>
-                ))}
-              </Nav>
-            </Navbar.Collapse>
-          )}
-        </Navbar>
-      </div>
-    )
-  }
+const Header = ({ categories }) => {
+  return (
+    <div className='app'>
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to='/'>Readable</Link>
+          </Navbar.Brand>
+          {categories && <Navbar.Toggle />}
+        </Navbar.Header>
+        {categories && (
+          <Navbar.Collapse>
+            <Nav pullRight>
+              {categories.map(c => (
+                <LinkContainer exact key={c.path} to={`/${c.path}`}>
+                  <NavItem>{c.name}</NavItem>
+                </LinkContainer>
+              ))}
+            </Nav>
+          </Navbar.Collapse>
+        )}
+      </Navbar>
+    </div>
+  )
 }
 
 export default Header
