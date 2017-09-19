0dbbf84cdf2381dafdeeafd4a9b008e13787df29
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-09-19 11:23:54 +0530

    Add back-end API server code

---
+++ back-end/default-comments.js
@@ -0,0 +1,82 @@
+module.exports = {
+  "894tuq4ut84ut8v4t8wun89g": {
+    id: '894tuq4ut84ut8v4t8wun89g',
+    parentId: "8xf0y6ziyjabvozdd253nd",
+    timestamp: 1468166872634,
+    body: 'Hi there! I am a COMMENT.',
+    author: 'thingtwo',
+    voteScore: 6,
+    deleted: false,
+    parentDeleted: false
+  },
+  "8tu4bsun805n8un48ve89": {
+    id: '8tu4bsun805n8un48ve89',
+    parentId: "8xf0y6ziyjabvozdd253nd",
+    timestamp: 1469479767190,
+    body: 'Comments. Are. Cool.',
+    author: 'thingone',
+    voteScore: -5,
+    deleted: false,
+    parentDeleted: false
+  },
+  "82704d8ef33345e698ee": {
+    "id": "82704d8ef33345e698ee",
+    "timestamp": 1505799863842,
+    "body": "That's true. You can't learn it in 10 minutes.",
+    "author": "true",
+    "parentId": "6ni6ok3ym7mf1p33lnez",
+    "voteScore": 10,
+    "deleted": false,
+    "parentDeleted": false
+  },
+  "0f080f98348d4ff58d01": {
+    "id": "0f080f98348d4ff58d01",
+    "timestamp": 1505799873056,
+    "body": "I can do it.",
+    "author": "possible",
+    "parentId": "6ni6ok3ym7mf1p33lnez",
+    "voteScore": -1000,
+    "deleted": false,
+    "parentDeleted": false
+  },
+  "dc490677bdce43cc9d27": {
+    "id": "dc490677bdce43cc9d27",
+    "timestamp": 1505799740291,
+    "body": "True. Even companies like Tesla use it.",
+    "author": "userone",
+    "parentId": "6883c8f4e4bd445da01c",
+    "voteScore": 8,
+    "deleted": false,
+    "parentDeleted": false
+  },
+  "03911f44c02e4794bf72": {
+    "id": "03911f44c02e4794bf72",
+    "timestamp": 1505799762193,
+    "body": "I don't use it.",
+    "author": "idiot",
+    "parentId": "6883c8f4e4bd445da01c",
+    "voteScore": -100,
+    "deleted": false,
+    "parentDeleted": false
+  },
+  "317e2ec7e95a465eac3a": {
+    "id": "317e2ec7e95a465eac3a",
+    "timestamp": 1505799945576,
+    "body": "That's great. I'm currently enrolled in the React Nanodegree.",
+    "author": "john",
+    "parentId": "7f49a7e3367c4eabb702",
+    "voteScore": 15,
+    "deleted": false,
+    "parentDeleted": false
+  },
+  "b5fb1510bb22449aa525": {
+    "id": "b5fb1510bb22449aa525",
+    "timestamp": 1505799958688,
+    "body": "I'm not enrolled in anything.",
+    "author": "haha",
+    "parentId": "7f49a7e3367c4eabb702",
+    "voteScore": -200,
+    "deleted": false,
+    "parentDeleted": false
+  }
+}
---
+++ back-end/default-posts.js
@@ -0,0 +1,42 @@
+module.exports = {
+  "8xf0y6ziyjabvozdd253nd": {
+    id: '8xf0y6ziyjabvozdd253nd',
+    timestamp: 1467166872634,
+    title: 'Udacity is the best place to learn React',
+    body: 'Everyone says so after all.',
+    author: 'thingtwo',
+    category: 'react',
+    voteScore: 6,
+    deleted: false
+  },
+  "6ni6ok3ym7mf1p33lnez": {
+    id: '6ni6ok3ym7mf1p33lnez',
+    timestamp: 1468479767190,
+    title: 'Learn Redux in 10 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false
+  },
+  "6883c8f4e4bd445da01c": {
+    "id": "6883c8f4e4bd445da01c",
+    "timestamp": 1505799433781,
+    "title": "A post about React Native",
+    "body": "Top companies like Facebook, Airbnb, Walmart use React Native for their apps.",
+    "author": "me",
+    "category": "react-native",
+    "voteScore": 3,
+    "deleted": false
+  },
+  "7f49a7e3367c4eabb702": {
+    "id": "7f49a7e3367c4eabb702",
+    "timestamp": 1505799523457,
+    "title": "A small description about Udacity",
+    "body": "Udacity can teach you the latest in Deep Learning, Machine Learning, Web Development & more.",
+    "author": "not-me",
+    "category": "udacity",
+    "voteScore": 1,
+    "deleted": false
+  }
+}
